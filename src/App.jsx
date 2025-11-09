0a335fbc69fc8024b8ab6eb6a058d4fef9ffd3a9
muhammad230 <ali.khansmr3@gmail.com>
2025-11-09 20:44:37 +0500

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+background-color: black;
+`;
 
 function App() {
   return (
     <>
-     
+     <button>this is button</button>
     </>
   )
 }
