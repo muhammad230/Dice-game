8687ea021f70918f04c36d8199aae6223930398f
muhammad230 <ali.khansmr3@gmail.com>
2025-11-10 20:19:55 +0500

    add css to home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,11 @@ import styled from "styled-components"
 const Home = () => {
   return (
     <Container>
-      <img src="/images/dices.png" alt="" />
       <div>
+        <img src="/images/dices.png" alt="" />
+      </div>
+      
+      <div className="contant">
         <h1>Dice Game</h1>
         <Button>Play Now</Button>
       </div>
@@ -14,8 +17,23 @@ const Home = () => {
 export default Home
 
 const Container = styled.div`
-max-width: 11880px;
+max-width: 1182px;
+display: flex;
+align-items: center;
+margin: 0 auto;
+height: 100vh;
+
+.contant h1{
+  font-size: 96px;
+  white-space: nowrap;
+}
 `;
 
 const Button = styled.button`
+padding: 10px 18px;
+background-color: #000;
+color: #ffff;
+border-radius: 5px;
+border: none;
+width: 220px;
 `;
