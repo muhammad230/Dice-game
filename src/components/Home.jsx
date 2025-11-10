e3d8bec35408e548d1d7a7df2da5e562020bdd52
muhammad230 <ali.khansmr3@gmail.com>
2025-11-10 20:27:44 +0500

    add hover to button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -36,4 +36,13 @@ color: #ffff;
 border-radius: 5px;
 border: none;
 width: 220px;
+font-size: 16px;
+ border: 1px solid white;
+ transition: 0.4s background ease-in;
+&:hover{
+  background-color: white;
+  border: 1px solid black;
+  color: black;
+  transition: 0.3s background ease-in;
+}
 `;
